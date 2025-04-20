a77934c0968b7ae865d30130f707e8543eedbdad
samyeuh <samybouhamidi2@gmail.com>
2025-04-20 23:29:46 +0200

    other profile

--- src/commands.py
+++ src/commands.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from discord import app_commands, Interaction
+from discord import app_commands, Interaction, Member
 from modal.link  import Link
 from embed import profile
 
@@ -19,9 +19,17 @@ class MusicboardCommands(commands.Cog):
     async def link(self, interaction: Interaction):
         await interaction.response.send_modal(Link(interaction.guild.id, interaction.user.id))
         
-    @app_commands.command(name="profile", description="see your musicboard profile")
-    async def profile(self, interaction: Interaction):
-        embed, view = profile.get_embed_info(interaction.guild.id, interaction.user.id, interaction.user.display_name)
+    @app_commands.command(name="profile", description="see a musicboard profile")
+    @app_commands.describe(user="the user whose profile you want to see (optional)")
+    async def profile(self, interaction: Interaction, user: Member = None):
+        target = user or interaction.user
+
+        embed, view = profile.get_embed_info(
+            guild_id=interaction.guild.id,
+            discord_id=target.id,
+            discord_name=target.display_name
+        )
+
         await interaction.response.send_message(embed=embed, view=view)
         
         
